73f9a219cf3f374bd40c59f8bac258f44b4a1def
Marshall Nevill <zach.nevill23@gmail.com>
2021-07-18 01:16:42 -0400

    add email validation to adoption form

--- kc.js
+++ kc.js
@@ -1,23 +1,15 @@
+const email = document.getElementById("email")
+const form = document.getElementById("form")
+const errorElement = document.getElementById("error")
 
-function validate() {
-    const email = document.getElementById("email").value
-    const emailValue = email
+form.addEventListener("submit" , (e) => {
+    let messages = []
+    if (email.value === "" || email.value === null) {
+        messages.push("email is required")
+    }
 
-if(emailValue === "null") {
-    setErrorFor(email, "Email cannot be blank");
-} else if(!isEmail(emailValue)) {
-    setErrorFor(email, "Email is not valid");
-} else {
-    setSuccessFor(email);
-};
-}
-
-
-function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small")
-    small.innerText = message;
-    formControl.className = "form-control error"}
-
-function isEmail(email) {
-    return /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i .test(email) };
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorElement.innerText = messages.join(", ")
+    }
+})
