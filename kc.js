8777f7fed0825b87dbae0dccc90773af0ca3d42c
Marshall Nevill <zach.nevill23@gmail.com>
2021-07-27 02:24:06 -0400

    updated JS to return users name in popup. also added updated notes to html and css sheets.

--- kc.js
+++ kc.js
@@ -1,32 +1,15 @@
-const email = document.getElementById("email")
-const form = document.getElementById("form")
-const errorElement = document.getElementById("error")
-
-form.addEventListener("submit" , (e) => {
-    let messages = []
-    if (email.value === "" || email.value === null) {
-        messages.push("email is required")
-    }
-
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(", ")
-    }
-})
-
-var alert = document.querySelector("email");
-
-alert.onclick = function () {
-    let message =("Did you enter the correct email?");
-    alert('You suggested ' + question + '! Check back for menu updates soon!');
-    if (question === null|| question === '') {
-        alert('Let us know if you think of something!');
-    }
+var name = document.getElementById("name")
+function getName() {
+    return document.getElementById("name").value;
 }
 
-function display()
-{
-alert("email");
-}
+function display() {
+    text = getName();
+    window.alert("Thank you for your application, " + text + "!");
+    document.getElementById("form").submit();
+} 
 
- 
+
+
+
+
